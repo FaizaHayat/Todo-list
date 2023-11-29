8586e284d7563c404ff9cd732d683b60d291839b
Faizaa Hayatt <hayatfaiza2@gmail.com>
2023-11-29 14:53:22 +0500

    Todo-list

--- src/componet/Textbutton.js
+++ src/componet/Textbutton.js
@@ -1,43 +1,88 @@
 import React, { useState } from 'react';
+import './Testbutton.css'
 
 function Textbutton() {
-
-
   const [todoList, setTodoList] = useState([]);
   const [currentTask, setCurrentTask] = useState('');
+  const [currentDescription, setCurrentDescription] = useState('');
+  const [editingIndex, setEditingIndex] = useState(null);
 
-  
   const handleInputChange = (event) => {
     setCurrentTask(event.target.value);
   };
 
+  const handleDescriptionChange = (event) => {
+    setCurrentDescription(event.target.value);
+  };
+
   const handleAddTask = () => {
     if (currentTask.trim() !== '') {
-      setTodoList([...todoList, currentTask]);
+      if (editingIndex !== null) {
+        const updatedTasks = [...todoList];
+        updatedTasks[editingIndex] = { task: currentTask, description: currentDescription };
+        setTodoList(updatedTasks);
+        setEditingIndex(null);
+      } else {
+        setTodoList([...todoList, { task: currentTask, description: currentDescription }]);
+      }
       setCurrentTask('');
+      setCurrentDescription('');
     }
   };
 
   const handleDeleteTask = (index) => {
-    const updatedTasks = todoList.filter((_, i) => i !== index);
+    const updatedTasks = [...todoList];
+    updatedTasks.splice(index, 1);
     setTodoList(updatedTasks);
+  };
+
+  const handleEditTask = (index) => {
+    const taskToEdit = todoList[index];
+    setCurrentTask(taskToEdit.task);
+    setCurrentDescription(taskToEdit.description);
+    setEditingIndex(index);
   };
 
   return (
     <div className="List-item">
       <h1>Todo List App</h1>
+      <div className='box'>
       <div className="task-input">
-        <input type="text" value={currentTask} onChange={handleInputChange} placeholder="Enter a new task" />
-        <button className="button" onClick={handleAddTask}>Add Task</button>
+        <label>Title</label>
+        <input className="input" type="text" value={currentTask} onChange={handleInputChange} placeholder="Enter a new task" />
       </div>
-      <div className="task-list">
-        {todoList.map((task, index) => (
-          <div key={index} className="task">
-            <p>{task}</p>
-            <button onClick={() => handleDeleteTask(index)}>Delete</button>
-          </div>
-        ))}
+      <div className="task-input">
+        <label>Description</label>
+        <input type="text"  value={currentDescription} onChange={handleDescriptionChange} placeholder="Enter description" />
+       
       </div>
+      <div className="task-input">
+      <button className="button" onClick={handleAddTask}>
+      {editingIndex !== null ? 'Update Task' : 'Add Task'}
+    </button></div>
+
+    </div>
+      <table className="task-list">
+        <thead>
+          <tr>
+            <th>Task Name</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todoList.map((task, index) => (
+            <tr key={index}>
+              <td>{task.task}</td>
+              <td>{task.description}</td>
+              <td>
+                <button onClick={() => handleDeleteTask(index)}>Delete</button>
+                <button onClick={() => handleEditTask(index)}>Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
